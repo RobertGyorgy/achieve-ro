d449aff87ec6511d75e3b8b76c9dc21f2548ca81
Robert Gyorgy <robertgyorgy@Laptop-Robert.local>
2025-07-14 22:54:02 +0300

    Remove import of App.css to fix PostCSS/Tailwind error

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Hero from './components/Hero'
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      {/* celelalte componente */}
+    </>
+  )
+}
+
